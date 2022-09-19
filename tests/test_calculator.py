04e7b53d8af97aad1181ba5e87b5d859065864a2
Andre <test@test.com>
2022-09-19 23:05:06 +0300

    modified

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,33 +3,35 @@ import calculator
 '''
 Sum
 * Int as input - return the int
-* int + floats - return the float
 * Strings as input - raise an exception
 
 Substraction
-* Int + int/floats as input - return the result
-* int + floats as input - return the float
+* Int as input - return the result
 * Strings as input - raise an exception
 
 Division
-* Int as input - return the result
-* int + floats as input - return the float
+* Int/floats as input - return the result
 * Strings as input - raise an exception
 * Ints/floats divided by 0 - raise ZeroDivision
 
 Multiply
 * Int/floats as input - return result
-* int + floats as input - return the float
 * Strings as input - raise an exception
 '''
 
 
-class TestCalculator(unittest.TestCase):
+class TestSumCalculator(unittest.TestCase):
     def test_sum_int(self):
         self.assertEqual(10, calculator.suma(5, 5))
 
-    def test_sum_floats(self):
+    def test_sum_float(self):
         self.assertEqual(10.5, calculator.suma(5.25, 5.25))
 
     def test_sum_string(self):
         self.assertRaises(TypeError, calculator.suma, 5, 'string')
+
+
+
+
+
+
