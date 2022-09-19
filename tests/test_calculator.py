e143506cdff274ab8e55f0fead978e2ff5989504
Andre <test@test.com>
2022-09-19 20:57:02 +0300

    added suma() in calculator.py and test_sum - 2 cases

---
+++ tests/test_calculator.py
@@ -0,0 +1,30 @@
+import unittest
+import calculator
+'''
+Sum
+* Int as input - return the int
+* Strings as input - raise an exception
+
+Substraction
+* Int as input - return the result
+* Strings as input - raise an exception
+
+Division
+* Int/floats as input - return the result
+* Strings as input - raise an exception
+* Ints/floats divided by 0 - raise ZeroDivision
+
+Multiply
+* Int/floats as input - return result
+* Strings as input - raise an exception
+'''
+
+
+class TestCalculator(unittest.TestCase):
+    def test_sum_int(self):
+        self.assertEqual(10, calculator.suma(5, 5))
+
+    def test_sum_string(self):
+        self.assertRaises(TypeError, calculator.suma, 5, 'string')
+
+
