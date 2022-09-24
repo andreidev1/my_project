335c23d640bdeeb2e7ca7f381909e406ce5b065a
Andre <test@test.com>
2022-09-24 18:29:48 +0300

    modified calculator.py

--- calculator.py
+++ calculator.py
@@ -1,2 +1,19 @@
+from decorators.checker import checker
+
+@checker
 def suma(x: int, y: int) -> int:
     return x + y
+
+@checker
+def substract(x: int, y: int) -> int:
+    return x - y
+
+@checker
+def divide(x: int, y: int) -> int:
+    return x // y
+
+@checker
+def multiply(x: int, y: int) -> int:
+    return x * y
+
+
