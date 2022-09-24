434d43c1dc0efa501bd5381d8b869a16a2059481
Andre <test@test.com>
2022-09-24 18:30:19 +0300

    add new tests

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,5 +1,6 @@
 import unittest
 import calculator
+
 '''
 Sum
 * Int as input - return the int
@@ -20,18 +21,23 @@ Multiply
 '''
 
 
-class TestSumCalculator(unittest.TestCase):
+class TestCalculator(unittest.TestCase):
+    # Sum Test
     def test_sum_int(self):
         self.assertEqual(10, calculator.suma(5, 5))
 
-    def test_sum_float(self):
-        self.assertEqual(10.5, calculator.suma(5.25, 5.25))
+    # Substract Test
+    def test_substract_int(self):
+        self.assertEqual(10, calculator.substract(20, 10))
 
-    def test_sum_string(self):
-        self.assertRaises(TypeError, calculator.suma, 5, 'string')
+    # Division Tests
+    def test_divide_int(self):
+        self.assertEqual(10, calculator.substract(20, 10))
 
+    def test_divide_by_zero(self):
+        self.assertRaises(ZeroDivisionError, calculator.divide, 5, 0)
 
-
-
-
+    # Multiply Test
+    def test_multiply(self):
+        self.assertEqual(30, calculator.multiply(6, 5))
 
